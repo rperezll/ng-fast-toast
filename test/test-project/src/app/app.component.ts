fd8872554a92d41f76782dab0eb4ef5a1d37e283
rperez <robertoprz898@gmail.com>
2024-12-21 15:11:12 +0000

    chore: rename sources path

---
+++ test/test-project/src/app/app.component.ts
@@ -0,0 +1,29 @@
+import { Component, inject, OnInit } from '@angular/core';
+import { NgFastToastComponent, NgFastToastService } from 'ng-fast-toast';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [NgFastToastComponent],
+  providers: [NgFastToastService],
+  templateUrl: './app.component.html'
+})
+export class AppComponent implements OnInit {
+  title = 'test-project';
+  testTypes = ['success', 'error', 'warning', 'loading'];
+
+  toast = inject(NgFastToastService);
+
+  ngOnInit() {
+      setTimeout(() => {
+        this.toast.success({content: 'ng-fast-toast are ready!', duration: 5});
+        setTimeout(() => {
+          this.toast.warn({content: 'ng-fast-toast are ready!', duration: 5});
+        }, 2000);
+        setTimeout(() => {
+          this.toast.error({content: 'ng-fast-toast are ready!', duration: 5});
+        }, 3000);
+      }, 1000);
+
+  }
+}
---
+++ test/test-project/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./**/*.{html,ts}",
+    "./node_modules/ng-fast-toast/**/*.{html,ts,js,mjs}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
