4516daf273fba5bc286dfb778cb0089e4113f607
rperez <robertoprz898@gmail.com>
2024-12-21 12:14:38 +0000

    test: check how an image displays in the README

--- showcase-project/projects/ng-fast-toast/src/public-api.ts
+++ showcase-project/projects/ng-fast-toast/src/public-api.ts
@@ -4,3 +4,6 @@
 
 export * from './services/ng-fast-toast.service';
 export * from './lib/ng-fast-toast.component';
+export * from './config/ng-fast-toast.config';
+export * from './interfaces/config.interface';
+export * from './interfaces/bg-color.interface';
