d69ea7c3451cae9927feb92f0c29b6ab47681bde
rperez <robertoprz898@gmail.com>
2024-12-21 12:12:29 +0000

    feat: add left/right alignment functionality for toast

--- showcase-project/projects/ng-fast-toast/src/lib/ng-fast-toast.component.ts
+++ showcase-project/projects/ng-fast-toast/src/lib/ng-fast-toast.component.ts
@@ -1,23 +1,28 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, Inject, inject, OnInit, Optional } from '@angular/core';
 import { NgFastToastService } from '../services/ng-fast-toast.service';
 import { ToastComponent } from '../ui/toast/toast.component';
 import { ToastConfig } from '../interfaces/notification-config.interface';
 import { generateGuid } from '../utils/generate-guid';
-import { calculateToastColors } from '../utils/color-types';
+import { calculateToastColors, calculateToastCustomColors } from '../utils/color-types';
+import { CommonModule } from '@angular/common';
+import { Config } from '../interfaces/config.interface';
 
 @Component({
 	selector: 'ng-fast-toast',
 	standalone: true,
-	imports: [ToastComponent],
+	imports: [ToastComponent, CommonModule],
 	templateUrl: './ng-fast-toast.component.html',
 })
 export class NgFastToastComponent implements OnInit {
 	notifications: ToastConfig[] = [];
 
-	ngFastToastService = inject(NgFastToastService);
+	constructor(
+		private ngFastToastService: NgFastToastService,
+		@Inject('ng-fast-toast-config') @Optional() public ngFastToastConfig?: Config,
+	) {}
 
 	ngOnInit(): void {
-		console.info('🍞 ng-fast-toast initialized correctly');
+		console.info(`🍞 ng-fast-toast initialized correctly with ${this.ngFastToastConfig ? 'custom config.' : 'default config.'}`);
 		this.notificationListener();
 	}
 
@@ -31,7 +36,7 @@ export class NgFastToastComponent implements OnInit {
 							content: data.notification.content,
 							title: data.notification.title,
 						},
-						colorConfig: calculateToastColors(data.type),
+						colorConfig: calculateToastCustomColors(this.ngFastToastConfig?.customToast ?? [], data.type),
 						duration: data.notification.duration,
 						type: data.type,
 					});
--- showcase-project/projects/ng-fast-toast/src/ui/toast/toast.component.ts
+++ showcase-project/projects/ng-fast-toast/src/ui/toast/toast.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, inject, Input, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, Inject, inject, Input, OnInit, Optional, Output } from '@angular/core';
 import { ToastConfig } from '../../interfaces/notification-config.interface';
 import { CircleProgressComponent } from '../progress-circle/circle-progress.component';
 import { secondsToMilliseconds } from '../../utils/time-parser';
@@ -6,13 +6,14 @@ import { CommonModule } from '@angular/common';
 import { WarningIconSvg } from '../../icons/warning/warning.component';
 import { SuccessIconSvg } from '../../icons/success/success.component';
 import { ErrorIconSvg } from '../../icons/error/error.component';
+import { Config } from '../../interfaces/config.interface';
 
 @Component({
 	selector: 'toast',
 	standalone: true,
 	imports: [CommonModule, CircleProgressComponent, WarningIconSvg, SuccessIconSvg, ErrorIconSvg],
 	styles: `
-		@keyframes slideIn {
+		@keyframes slideInRight {
 			0% {
 				transform: translateX(100%);
 				opacity: 0;
@@ -23,7 +24,7 @@ import { ErrorIconSvg } from '../../icons/error/error.component';
 			}
 		}
 
-		@keyframes slideOut {
+		@keyframes slideOutRight {
 			0% {
 				transform: translateX(0);
 				opacity: 1;
@@ -34,12 +35,42 @@ import { ErrorIconSvg } from '../../icons/error/error.component';
 			}
 		}
 
-		.toast-enter {
-			animation: slideIn 0.5s forwards;
+		@keyframes slideInLeft {
+			0% {
+				transform: translateX(-100%);
+				opacity: 0;
+			}
+			100% {
+				transform: translateX(0);
+				opacity: 1;
+			}
 		}
 
-		.toast-exit {
-			animation: slideOut 0.5s forwards;
+		@keyframes slideOutLeft {
+			0% {
+				transform: translateX(0);
+				opacity: 1;
+			}
+			100% {
+				transform: translateX(-100%);
+				opacity: 0;
+			}
+		}
+
+		.toast-enter-right {
+			animation: slideInRight 0.5s forwards;
+		}
+
+		.toast-exit-right {
+			animation: slideOutRight 0.5s forwards;
+		}
+
+		.toast-enter-left {
+			animation: slideInLeft 0.5s forwards;
+		}
+
+		.toast-exit-left {
+			animation: slideOutLeft 0.5s forwards;
 		}
 	`,
 	templateUrl: './toast.component.html',
@@ -48,8 +79,12 @@ export class ToastComponent implements OnInit {
 	@Input({ required: true }) config: ToastConfig;
 	@Output() finish = new EventEmitter<string>();
 
+	constructor(@Inject('ng-fast-toast-config') @Optional() public ngFastToastConfig?: Config) {}
+
 	ngOnInit(): void {
 		this.toastCicle();
+
+		console.log(this.config.colorConfig.bgToastColor);
 	}
 
 	private toastCicle() {
@@ -57,11 +92,20 @@ export class ToastComponent implements OnInit {
 		setTimeout(() => {
 			const alertElement = document.getElementById(this.config.guid);
 			alertElement.classList.remove('opacity-0');
-			alertElement.classList.add('toast-enter');
+			if (this.ngFastToastConfig?.align === 'left') {
+				alertElement.classList.add('toast-enter-left');
+			} else {
+				alertElement.classList.add('toast-enter-right');
+			}
 			// Exit waiting time setting form user
 			setTimeout(() => {
-				alertElement.classList.remove('toast-enter');
-				alertElement.classList.add('toast-exit');
+				if (this.ngFastToastConfig?.align === 'left') {
+					alertElement.classList.remove('toast-enter-left');
+					alertElement.classList.add('toast-exit-left');
+				} else {
+					alertElement.classList.remove('toast-enter-right');
+					alertElement.classList.add('toast-exit-right');
+				}
 				// Waiting time for remove notification
 				setTimeout(() => {
 					this.finish.emit(this.config.guid);
