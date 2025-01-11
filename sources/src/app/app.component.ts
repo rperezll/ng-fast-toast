b818765208d21762649ef3810f3f077f5bc6379b
rperez <robertoprz898@gmail.com>
2025-01-11 12:04:36 +0000

    fix: warn toast duration test

--- sources/src/app/app.component.ts
+++ sources/src/app/app.component.ts
@@ -33,7 +33,7 @@ export class AppComponent {
 					title: 'Listen!',
 					content:
 						'A lightweight and fast notification library for Angular, designed to make it super easy to display quick, stylish alerts (toasts) in your app. With a simple and customizable API, you can easily control the look, position, and behavior of your notifications.',
-					duration: 5000,
+					duration: 5,
 				});
 				break;
 			case 'error':
