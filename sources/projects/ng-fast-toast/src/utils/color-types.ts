f7eb86c01e24befe9c269d5fd1f4a2ba54ac0a06
rperez <robertoprz898@gmail.com>
2025-02-01 17:01:43 +0000

    feat: added new default color sets

--- sources/projects/ng-fast-toast/src/utils/color-types.ts
+++ sources/projects/ng-fast-toast/src/utils/color-types.ts
@@ -5,47 +5,46 @@
  * See the LICENSE file in the root directory for more information.
  */
 
-
 import { BgColorTypes } from '../interfaces/bg-color.interface';
 import type { NotificationType } from '../types/notification.type';
 
 export const ColorTypes: BgColorTypes[] = [
 	{
-	  type: "error",
-	  bgToastColor: "#fee2e2",  // bg-red-50
-	  borderToastColor: "#f87171",  // border-red-500
-	  titleSvgColor: "#991b1b",  // #991b1b
-	  titleTextColor: "#b91c1c",  // text-red-800
-	  contentTextColor: "#c92a2a",  // text-red-700
-	  progressCircleColor: "#b91c1c"  // text-red-800
+		type: 'error',
+		bgToastColor: '#fee2e2', // bg-red-50
+		borderToastColor: '#f87171', // border-red-500
+		titleSvgColor: '#991b1b', // #991b1b
+		titleTextColor: '#b91c1c', // text-red-800
+		contentTextColor: '#c92a2a', // text-red-700
+		progressCircleColor: '#b91c1c', // text-red-800
 	},
 	{
-	  type: "warning",
-	  bgToastColor: "#fef3c7",  // bg-yellow-50
-	  borderToastColor: "#f59e0b",  // border-yellow-500
-	  titleSvgColor: "#854d0e",  // #854d0e
-	  titleTextColor: "#b45309",  // text-yellow-800
-	  contentTextColor: "#c26a00",  // text-yellow-700
-	  progressCircleColor: "#b45309"  // text-yellow-800
+		type: 'warning',
+		bgToastColor: '#fef3c7', // bg-yellow-50
+		borderToastColor: '#f59e0b', // border-yellow-500
+		titleSvgColor: '#854d0e', // #854d0e
+		titleTextColor: '#b45309', // text-yellow-800
+		contentTextColor: '#c26a00', // text-yellow-700
+		progressCircleColor: '#b45309', // text-yellow-800
 	},
 	{
-	  type: "success",
-	  bgToastColor: "#d1fae5",  // bg-green-50
-	  borderToastColor: "#22c55e",  // border-green-500
-	  titleSvgColor: "#166534",  // #166534
-	  titleTextColor: "#15803d",  // text-green-800
-	  contentTextColor: "#16a34a",  // text-green-700
-	  progressCircleColor: "#15803d"  // text-green-800
+		type: 'success',
+		bgToastColor: '#d1fae5', // bg-green-50
+		borderToastColor: '#22c55e', // border-green-500
+		titleSvgColor: '#166534', // #166534
+		titleTextColor: '#15803d', // text-green-800
+		contentTextColor: '#16a34a', // text-green-700
+		progressCircleColor: '#15803d', // text-green-800
 	},
 	{
-	  type: "loading",
-	  bgToastColor: "#d1fae5",  // bg-green-50
-	  borderToastColor: "#22c55e",  // border-green-500
-	  titleSvgColor: "#166534",  // #166534
-	  titleTextColor: "#15803d",  // text-green-800
-	  contentTextColor: "#16a34a",  // text-green-700
-	  progressCircleColor: "#15803d"  // text-green-800
-	}
+		type: 'loading',
+		bgToastColor: '#f7fafc',
+		borderToastColor: '#cbd5e0',
+		titleSvgColor: '#718096',
+		titleTextColor: '#718096',
+		contentTextColor: '#718096',
+		progressCircleColor: '#15803d',
+	},
 ];
 
 export function calculateToastColors(type: NotificationType): BgColorTypes {
